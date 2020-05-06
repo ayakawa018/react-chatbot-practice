8e710e590eb1170fb319d5ad86f8981db058d19f
川崎 彩香 <au0108@gmail.com>
2020-05-06 20:28:56 +0900

    ⚡️ add:chat-area

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import defaultDataset from "./dataset";
 import './assets/styles/style.css';
 import  './assets/styles/index.css';
-import {AnswerList, Chat, Chats} from "./components";
+import {AnswerList, Chats} from "./components";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -37,6 +37,12 @@ export default class App extends React.Component {
       case (nextQuestionId === 'init'):
         this.displayNextQuestion(nextQuestionId)
         break;
+      case (/https:*/.test(nextQuestionId)):
+        const a = document.createElement('a');
+        a.href = nextQuestionId;
+        a.target = '_blank';
+        a.click();
+        break;
       default:
         const chats = this.state.chats;
         chats.push({
@@ -48,7 +54,7 @@ export default class App extends React.Component {
           chats: chats
         })
 
-        this.displayNextQuestion(nextQuestionId)
+        setTimeout( ()=>this.displayNextQuestion(nextQuestionId),500)
         break;
     }
   }
@@ -56,6 +62,13 @@ export default class App extends React.Component {
   componentDidMount() {
     const initAnswer = "";
     this.selectAnswer(initAnswer, this.state.currentId)
+  }
+
+  componentDidUpdate() {
+    const scrollArea = document.getElementById('scroll-area')
+    if(scrollArea) {
+      scrollArea.scrollTop = scrollArea.scrollHeight
+    }
   }
 
   render() {
--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,19 +1,28 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-    },
-  },
-}));
+const useStyles = makeStyles(() => (
+  createStyles({
+    "button": {
+      backgroundColor: "#fff",
+      borderColor: "#ffb549",
+      color: "#ffb549",
+      fontWeight: 600,
+      marginBottom: "8px",
+      transition: "backgroundcolor .3s , color .3s",
+      "&:hover": {
+        backgroundColor: "#ffb549",
+        color: "#fff"
+      }
+    }
+  })
+));
 
 const Answer = (props) => {
   const classes = useStyles();
   return (
-    <Button variant="contained" color="secondary" onClick={() => props.select(props.content, props.nextId)}>
+    <Button className={classes.button} variant="outlined" onClick={() => props.select(props.content, props.nextId)}>
       {props.content}
     </Button>
   )
