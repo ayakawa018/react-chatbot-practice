83d7432885f266d79e9af1e3f9bea6af9731cb12
川崎 彩香 <au0108@gmail.com>
2020-06-07 22:56:04 +0900

    modify:database fetch by firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import defaultDataset from "./dataset";
+// import defaultDataset from "./dataset";
 import './assets/styles/style.css';
 import  './assets/styles/index.css';
 import {AnswerList, Chats} from "./components";
 import FormDialog from "./components/Forms/FormDialog";
+import {db} from './firebase/index'
 
 export default class App extends React.Component {
   constructor(props) {
@@ -12,7 +13,7 @@ export default class App extends React.Component {
       answers: [],
       chats: [],
       currentId: "init",
-      dataset: defaultDataset,
+      dataset: {},
       open: false,
     }
     this.selectAnswer = this.selectAnswer.bind(this)
@@ -75,10 +76,27 @@ export default class App extends React.Component {
     })
   }
 
+  initDataset = (dataset) => {
+    this.setState( {dataset: dataset});
+}
+
   componentDidMount() {
-    const initAnswer = "";
-    this.selectAnswer(initAnswer, this.state.currentId)
-  }
+    (async () => {
+      const dataset = this.state.dataset;
+
+      await db.collection('questions').get().then(snapshots => {
+        snapshots.forEach(doc => {
+          const id = doc.id;
+          const data = doc.data();
+          dataset[id] = data;
+        })
+      })
+
+      this.initDataset(dataset);
+      const initAnswer = "";
+      this.selectAnswer(initAnswer, this.state.currentId)
+    })();
+  };
 
   componentDidUpdate() {
     const scrollArea = document.getElementById('scroll-area')
---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAOQVeSqGzFKGo0lt6x-K0jdFNomZlMVg4",
+  authDomain: "react-chatbot-prctice.firebaseapp.com",
+  databaseURL: "https://react-chatbot-prctice.firebaseio.com",
+  projectId: "react-chatbot-prctice",
+  storageBucket: "react-chatbot-prctice.appspot.com",
+  messagingSenderId: "643282545077",
+  appId: "1:643282545077:web:07346c782d718f62d651fc",
+  measurementId: "G-FK0Q3PDQW8"
+};
+
+export default firebaseConfig
